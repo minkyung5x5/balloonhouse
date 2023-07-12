cb20f7ec753a8c20b4ae67edd91fbcdc1a6ce7e1
minkyung5x5 <minkyung5x5@gmail.com>
2023-07-12 10:53:34 +0900

    add styled-components, removeBalloon

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import styles from "./App.module.css"
 import Balloon from "./Balloon"
 import House from "./House"
-
+import styled, { createGlobalStyle } from 'styled-components';
 
 function App() {
   return (
-    <div className={styles.sky}>
-      <Balloon />
-      <House />
+    <div>
+      <GlobalStyle />
+      <Sky>
+        <Balloon />
+        <House />
+      </Sky>
     </div>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+const Sky = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(rgb(255, 141, 141), rgb(33, 59, 255));
+`;
 
 export default App;
